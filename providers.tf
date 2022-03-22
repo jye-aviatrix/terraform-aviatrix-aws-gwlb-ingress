089fe20b4fa68c3f32b03a0c37e9e87ec978c91c
Jun Ye <jye@aviatrix.com>
2022-03-22 11:01:42 -0400

    Update variables

--- providers.tf
+++ providers.tf
@@ -1,9 +1,6 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
+
     aviatrix = {
       source = "AviatrixSystems/aviatrix"
       version = "2.21.1-6.6.ga"
